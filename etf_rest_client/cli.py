98f8c89c49e54dd2daa41c9fa8ca252f7f52b40b
Anton Bakker <anton.bakker@kadaster.nl>
2020-04-23 08:56:44 +0200

    update requests package

--- etf_rest_client/cli.py
+++ etf_rest_client/cli.py
@@ -13,9 +13,9 @@ def cli():
 @click.argument('service-url')
 @click.argument('result-path', type=click.Path(exists=True))
 @click.option('--validator-url')
-def validate_atom_commmand(
+def validate_service_command(
         service_type, service_url, result_path, validator_url):
-    result = validate_service("ATOM", service_url, result_path, validator_url)
+    result = validate_service(service_type, service_url, result_path, validator_url)
     print(json.dumps(result, indent=4))
 
 
